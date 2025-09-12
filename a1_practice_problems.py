dd6299be31ce43cd1d0e5db640ce86e708cb14b5
Levi <lrrice1@cps.edu>
2025-09-12 10:25:02 -0500

    transfered to new repository

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -1,125 +1,178 @@
 """Assignment 1
 
-Fill in the following function skeletons - descriptions are provided in 
+
+Fill in the following function skeletons - descriptions are provided in
 the docstring (the triple quote thing at the top of each function)
 
+
 Some assert statements have been provided - write more of them to test your code!
+
 
 The `raise NotImplementedError(...)`s are placeholders to help you not skip implementing
 a function. They should be removed and replaced with your solution.
 
-This portion of the assignment will not be graded, but this gives you some problems to 
+
+This portion of the assignment will not be graded, but this gives you some problems to
 check and we'll be doing them in class.
+
 
 Make sure to complete the a1.py problems which should be AI generated.
 """
 
+
 from typing import List, TypeVar
+
+
 
 
 def absolute(n: int) -> int:
     """Gives the absolute value of the passed in number. Cannot use the built in
     function `abs`.
 
+
     Args:
         n - the number to take the absolute value of
+
 
     Returns:
         the absolute value of the passed in number
     """
-    raise NotImplementedError("absolute")
+    if n < 0:
+        return -1 * n
+    return n
+
+
 
 
 def factorial(n: int) -> int:
     """Takes a number n, and computes the factorial n! You can assume the passed in
     number will be positive
 
+
     Args:
         n - the number to compute factorial of
+
 
     Returns:
         factorial of the passed in number
     """
-    raise NotImplementedError("factorial")
+    result = 1
+    for num in range(1, n+1):
+        result = result * num
+    return result
 
 
 T = TypeVar("T")
+
+
 
 
 def every_other(lst: List[T]) -> List[T]:
     """Takes a list and returns a list of every other element in the list, starting with
     the first.
 
+
     Args:
         lst - a list of any (constrained by type T to be the same type as the returned
             list)
 
+
     Returns:
         a list of every of other item in the original list starting with the first
     """
-    raise NotImplementedError("every_other")
+    return lst[::2]
+
+
 
 
 def sum_list(lst: List[int]) -> int:
     """Takes a list of numbers, and returns the sum of the numbers in that list. Cannot
     use the built in function `sum`.
 
+
     Args:
         lst - a list of numbers
+
 
     Returns:
         the sum of the passed in list
     """
-    raise NotImplementedError("sum_list")
+    total = 0
+    for element in lst:
+        total += element
+    return total
+
+
 
 
 def mean(lst: List[int]) -> float:
     """Takes a list of numbers, and returns the mean of the numbers.
 
+
     Args:
         lst - a list of numbers
+
 
     Returns:
         the mean of the passed in list
     """
-    raise NotImplementedError("mean")
+    return sum(lst) / len(lst) if lst else 0
+
+
 
 
 def median(lst: List[int]) -> float:
     """Takes an ordered list of numbers, and returns the median of the numbers.
 
+
     If the list has an even number of values, it computes the mean of the two center
     values.
+
 
     Args:
         lst - an ordered list of numbers
 
+
     Returns:
         the median of the passed in list
     """
-    raise NotImplementedError("median")
+    if len(lst) % 2 == 1:
+        return lst[len(lst) // 2]
+    else:
+        m1 = len(lst) / 2
+        m2 = m1 - 1
+        return (lst[m1] + lst[m2]) / 2
+
+
 
 
 def duck_duck_goose(lst: List[str]) -> List[str]:
     """Given an list of names (strings), play 'duck duck goose' with it, knocking out
     every third name (wrapping around) until only two names are left.
 
+
     In other words, when you hit the end of the list, wrap around and keep counting from
     where you were.
+
 
     For example, if given this list ["roscoe", "kim", "woz", "solin"], you'd first
     knock out woz. Then first 'duck' on solin, wrap around to 'duck' on roscoe and
     'goose' on kim - knocking him out and leaving only roscoe and solin.
 
+
     You may assume the list has 3+ names to start
+
 
     Args:
         lst - a list of names (strings)
+
 
     Returns:
         the resulting list after playing duck duck goose
     """
     raise NotImplementedError("duck_duck_goose")
+
+
 
 
 # this line causes the nested code to be skipped if the file is imported instead of run
@@ -135,7 +188,11 @@ if __name__ == "__main__":
     assert mean([1, 2, 3, 4, 5]) == 3, "mean of [1,2,3,4,5] failed"
     assert median([1, 2, 3, 4, 5]) == 3, "median of [1,2,3,4,5] failed"
 
+
     names = ["roscoe", "kim", "woz", "solin", "law", "remess"]
     assert duck_duck_goose(names) == ["roscoe", "law"]
 
+
     print("All tests passed!")
+
+
